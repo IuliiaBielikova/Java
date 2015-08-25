0b0a541c3c6af638a61daf19e397be61611153e0
IuliiaBielikova <j.belikova89@gmail.com>
2015-08-25 13:33:56 +0300

    *

--- Foundation18/src/week2/MaxMinVector.java
+++ Foundation18/src/week2/MaxMinVector.java
@@ -15,22 +15,23 @@ public static void swapMaxMinVector(int[] vector) {
 	for(int i=0; i<vector.length; i++) {
 	vector[i]=(int)(Math.random()*10);
 			}
-	for (int i=0; i<vector.length; i++) {
-	System.out.print(vector[i]+" ");
-		}
-	System.out.println();
-	
-	int max = 1;
-    int min = 0;
-    for(int i = 1; i < vector.length; i++){
-        if(vector[i]>max){
-            max=vector[i];}
-        }
-    for(int i = 1; i < vector.length; i++){
-        if(vector[i]<vector[i-1]){
-            min=vector[i];}
-        }
-    System.out.println(min);
-    System.out.println(max);
+//	for (int i=0; i<vector.length; i++) {
+//	System.out.print(vector[i]+" ");
+//		}
+//	System.out.println();
+	int maxIndex = 0;
+    int minIndex = 0;
+    for (int i = 0; i < vector.length; i++) {
+     if (vector[i] > vector[maxIndex])
+      maxIndex = i;
+     if (vector[i] < vector[minIndex])
+      minIndex = i;
+    }
+    int temp = vector[maxIndex];
+    vector[maxIndex] = vector[minIndex];
+    vector[minIndex] = temp;
+    for (int i=0; i<vector.length; i++) {
+    System.out.print(vector[i]+" ");
+    	}
 	}
 }
